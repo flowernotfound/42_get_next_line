f95e3f5682a00f65bbcb4bfb06d3b4865c615185
Hiroaki Mitsuyoshi <thnksfrth3244@gmail.com>
2023-10-01 06:17:17 +0900

    created mandatory files

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: hmitsuyo <yourLogin@student.42.fr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/30 19:46:25 by hmitsuyo          #+#    #+#             */
-/*   Updated: 2023/09/30 19:47:08 by hmitsuyo         ###   ########.fr       */
+/*   Updated: 2023/10/01 06:16:37 by hmitsuyo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,4 +14,12 @@
 
 char	*get_next_line(int fd)
 {
+	char	*buf;
+
+	if (fd < 0 || BUFFER_SIZE < 0)
+		return (NULL);
+	buf = (char *)malloc(sizeof(char) * (BUFFER_SIZE + 1));
+	if (buf == NULL)
+		return (NULL);
+	read(fd, buf, BUFFER_SIZE);
 }
--- get_next_line.h
+++ get_next_line.h
@@ -6,8 +6,14 @@
 /*   By: hmitsuyo <yourLogin@student.42.fr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/30 19:47:20 by hmitsuyo          #+#    #+#             */
-/*   Updated: 2023/09/30 19:47:25 by hmitsuyo         ###   ########.fr       */
+/*   Updated: 2023/10/01 00:50:46 by hmitsuyo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef GET_NEXT_LINE
+# define GET_NEXT_LINE
 
+# include <unistd.h>
+# include <stdlib.h>
+
+#endif
