6cf6f1479f59b29fa11f506b754e56c6bdce5291
Hiroaki Mitsuyoshi <thnksfrth3244@gmail.com>
2023-10-01 17:20:38 +0900

    updating mandatory

--- get_next_line.c
+++ get_next_line.c
@@ -6,14 +6,18 @@
 /*   By: hmitsuyo <yourLogin@student.42.fr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/30 19:46:25 by hmitsuyo          #+#    #+#             */
-/*   Updated: 2023/10/01 06:16:37 by hmitsuyo         ###   ########.fr       */
+/*   Updated: 2023/10/01 12:04:41 by hmitsuyo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
+
+
 char	*get_next_line(int fd)
 {
+	static char	*s;
+	ssize_t	read_bytes;
 	char	*buf;
 
 	if (fd < 0 || BUFFER_SIZE < 0)
@@ -21,5 +25,11 @@ char	*get_next_line(int fd)
 	buf = (char *)malloc(sizeof(char) * (BUFFER_SIZE + 1));
 	if (buf == NULL)
 		return (NULL);
-	read(fd, buf, BUFFER_SIZE);
+	read_bytes = read(fd, buf, BUFFER_SIZE);
+	if (read_bytes == -1)
+	{
+		free(buf);
+		return (NULL);
+	}
+
 }
